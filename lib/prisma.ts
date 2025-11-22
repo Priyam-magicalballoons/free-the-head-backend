e41fc218e045f2f4e0c2d215b15f177980fe47c4
B-priyam <priyam3801h@gmail.com>
2025-11-22 14:23:42 +0530

    initial commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,16 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+declare global {
+  // Prevent multiple PrismaClient instances in dev
+  // (Next.js hot reload can create many instances)
+  var prisma: PrismaClient | undefined;
+}
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma =
-  globalForPrisma.prisma ||
+export const prisma =
+  global.prisma ||
   new PrismaClient({
-    adapter,
+    datasources: { db: { url: process.env.DATABASE_URL } },
+    log: ["error", "warn"], // optional
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
