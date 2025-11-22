cbe5f5243b423ae4e302757c124228e167dd2f26
B-priyam <priyam3801h@gmail.com>
2025-11-22 14:04:18 +0530

    initial commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,10 @@
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+  const allUsers = await prisma.user.findMany();
   return NextResponse.json({
+    allUsers,
     hello: "world",
   });
 }
